cfc2a8ae7c9f3d804a3311a06307c201e43e1c71
hysun308 <ohare009@gmail.com>
2016-11-10 11:20:40 -0800

    update

---
+++ census_classifier.py
@@ -0,0 +1,6 @@
+from mnist import MNIST
+import sklearn.metrics as metrics
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+import scipy
---
+++ spam_classifier.py
@@ -0,0 +1,7 @@
+from mnist import MNIST
+import sklearn.metrics as metrics
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+import scipy
+
